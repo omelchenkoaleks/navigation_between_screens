b2a4a4c82e4b763c3e41c1dda7e610e2eec62b56
omelchenkoaleks <omelchenkoaleks@gmail.com>
2022-01-07 07:12:49 +0200

    using app router

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,7 @@ import 'package:provider/provider.dart';
 
 import 'fooderlich_theme.dart';
 import 'models/models.dart';
-import 'screens/splash_screen.dart';
-// TODO: Import app_router
+import 'navigation/app_router.dart';
 
 void main() {
   runApp(
@@ -23,9 +22,17 @@ class _FooderlichState extends State<Fooderlich> {
   final _groceryManager = GroceryManager();
   final _profileManager = ProfileManager();
   final _appStateManager = AppStateManager();
-  // TODO: Define AppRouter
+  late AppRouter _appRouter;
 
-  // TODO: Initialize app router
+  @override
+  void initState() {
+    super.initState();
+    _appRouter = AppRouter(
+      appStateManager: _appStateManager,
+      groceryManager: _groceryManager,
+      profileManager: _profileManager,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,8 +60,10 @@ class _FooderlichState extends State<Fooderlich> {
           return MaterialApp(
             theme: theme,
             title: 'Fooderlich',
-            // TODO: Replace with Router widget
-            home: const SplashScreen(),
+            home: Router(
+              routerDelegate: _appRouter,
+              // TODO: Add backButtonDispatcher
+            ),
           );
         },
       ),
